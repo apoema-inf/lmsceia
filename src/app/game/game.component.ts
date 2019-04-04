a95372565469b314b42cb11ceb219236f12e1222
Hyago Souza <hyagosouzza@hotmail.com>
2019-04-04 12:57:02 -0300

    alterações hernane

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -20,6 +20,7 @@ export class GameComponent implements OnInit {
   sexta: boolean = false;
   setima: boolean = false;
   oitava: boolean = false;
+  lideres: Array<any> = new Array();
 
   public barChartOptions: ChartOptions = {
     responsive: true,
@@ -68,7 +69,18 @@ export class GameComponent implements OnInit {
   public barChartLegend = true;
 
   constructor(private af: AngularFirestore, private fbs: FirebaseService) {
-    this.getAtividadesTime();
+    /* this.getAtividadesTime(); */
+    this.lideres.push({nome: 'Francisco Lucas da Silva', time: 'Time #01', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-01%2FFrancisco%20Lucas%20da%20Silva%20-%20AsaNoturna.png?alt=media&token=2c4e3b01-22ef-4082-a2af-d69a5e06c446'});
+    this.lideres.push({nome: 'Felipe Neves Rocha da Silva', time: 'Time #02', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-02%2FFelipe%20-%20Sir_Lipitos.png?alt=media&token=3de5aeb7-1ad7-4cfa-8467-fa5b2de1b7d2'});
+    this.lideres.push({nome: 'Gadiel Ribeiro De Almeida', time: 'Time #03', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-03%2FGadigas.jpg?alt=media&token=64e14be8-e641-4477-b69d-e5aac91a564e'});
+    this.lideres.push({nome: 'Lauriana Alves Moreira', time: 'Time #04', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-04%2FLaur%C3%ADssima.jpeg?alt=media&token=0e4b9772-4ce1-45bf-8764-6914fb597284'});
+    this.lideres.push({nome: 'Fernando Teles Da Silva Mesquita', time: 'Time #05', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-05%2F%20Fernando%20Teles%20-%20Teles.jpeg?alt=media&token=c27abc5c-3423-4960-9d6a-5ed6d89b09e2'});
+    this.lideres.push({nome: 'Rodolfo Wenceslau Mota', time: 'Time #06', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-06%2FRodolfo%20Wenceslau.jpeg?alt=media&token=edc2d2bc-bf9b-4186-8060-9aabbdab5b77'});
+    this.lideres.push({nome: 'Eduardo Koelln', time: 'Time #07', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-07%2FMagr%C3%A3o.jpeg?alt=media&token=c5b91cfd-b198-44f7-b98e-54a978cbd45b'});
+    this.lideres.push({nome: 'Lorena Brandão Romeiro', time: 'Time #09', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-09%2FAvatar%20-%20Loris%20-%20Lorena.jpg?alt=media&token=285802e0-01f8-4419-ab57-bb158cefac42'});
+    this.lideres.push({nome: 'Anderson de Oliveira Costa', time: 'Time #10', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-10%2FAnderson-TCHUY.jpeg?alt=media&token=8b95537d-d237-4a79-8382-192822109ef8'});
+    this.lideres.push({nome: 'Matheus Silva Santos', time: 'Time #11', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-11%2FMatheus%20Santos.png?alt=media&token=2705bbd8-aee3-4e5b-aaf6-7ff80dbfab81'});
+    this.lideres.push({nome: 'Franceles Bezerra De Oliveira', time: 'Time #12', img: 'https://firebasestorage.googleapis.com/v0/b/posgrad.appspot.com/o/jogadores%2Ftime-12%2FFranceles%20ser%C3%A1%20Nubes%20(nuvem).jpeg?alt=media&token=74283092-5cd5-4399-bc82-aecc54bf68d2'});
   }
 
   ngOnInit() {
@@ -179,7 +191,7 @@ export class GameComponent implements OnInit {
     }
   }
 
-  getAtividadesTime() {
+  /* getAtividadesTime() {
     let pontuacao = 0;
     let nomeTime;
     let arrayTimes = [];
@@ -207,6 +219,6 @@ export class GameComponent implements OnInit {
         this.barChartData[0].data.push(data.pontuacao);
       });
     });
-  }
+  } */
 
 }
--- src/app/models/missao.model.ts
+++ src/app/models/missao.model.ts
@@ -1,9 +1,9 @@
 import { Fase } from "./fase.model";
 
 export class Missao {
-    id: string;
-    nome: string;
-    descricao: string;
-    temporada: Fase;
+  id: string;
+  nome: string;
+  descricao: string;
+  temporada: Fase;
   order: any;
 }
